00fcbc0ecca133375ae8743b2fd9e811d1b8ac74
Andrei Dubinko <andreidubinko@Andreis-MacBook-Pro.local>
2021-10-24 22:04:45 +0300

    final fixes 3

--- js/script.js
+++ js/script.js
@@ -18,113 +18,124 @@ calculate_btn.addEventListener('click', function(e){
     let health_day_compensation = 0;
     let day_compensation = 0;
 
-    e.preventDefault();
-    let income = income_inp.value;
-    let day = days.value;
-    total_days.innerHTML = day;
-    if(checkbox.checked){
-        let no_tubercolosis = Number(income * 6) / 240;
-        let deduction30 =  no_tubercolosis - (no_tubercolosis * 0.3);
-        let dayCompensation =  (deduction30 - (deduction30 * 0.2)).toFixed(2);
-        health_day_compensation = dayCompensation;
-        day_compensation = dayCompensation;
-        if (Number(day) <= 7){
-            employer_compensation.innerHTML = (day_compensation * Number(day - 3)).toFixed(2);
-        } else {
-            employer_compensation.innerHTML = (day_compensation * 4).toFixed(2);
-        }
-        employer_daily.innerHTML = dayCompensation;
-        final_price.innerHTML = ((day_compensation * Number(day - 3)).toFixed(2));
-        if(day > 240){
-            health_daily.innerHTML = '0,00';
-            employer_daily.innerHTML = '0,00';
-            final_price.innerHTML = '0.00';
-        }
-        daysCalc(day);
+    if(income_inp.value == ''){
+        income_inp.classList.add('alert-danger');
+    } else if (days.value == '') {
+        days.classList.add('alert-danger');
+        income_inp.classList.remove('alert-danger');
     } else {
-        let no_tubercolosis = Number(income * 6) / 182;
-        let deduction30 =  no_tubercolosis - (no_tubercolosis * 0.3);
-        let dayCompensation =  (deduction30 - (deduction30 * 0.2)).toFixed(2);
-        health_day_compensation = dayCompensation;
-        day_compensation = dayCompensation;
-        if (Number(day) <= 7){
-            employer_compensation.innerHTML = (day_compensation * Number(day - 3)).toFixed(2);
-        } else {
-            employer_compensation.innerHTML = (day_compensation * 4).toFixed(2);
-        }
-        employer_daily.innerHTML = dayCompensation;
-        final_price.innerHTML = ((day_compensation * Number(day - 3)).toFixed(2));
-        if(day > 182){
-            health_daily.innerHTML = '0,00';
-            employer_daily.innerHTML = '0,00';
-            final_price.innerHTML = '0.00';
-        }
-        daysCalc(day);
-    }
+        income_inp.classList.remove('alert-danger');
+        days.classList.remove('alert-danger');
 
-    function daysCalc(num){
+        e.preventDefault();
+        let income = income_inp.value;
+        let day = days.value;
+        total_days.innerHTML = day;
         if(checkbox.checked){
-            if(num <= 3){
+            let no_tubercolosis = Number(income * 6) / 240;
+            let deduction30 =  no_tubercolosis - (no_tubercolosis * 0.3);
+            let dayCompensation =  (deduction30 - (deduction30 * 0.2)).toFixed(2);
+            health_day_compensation = dayCompensation;
+            day_compensation = dayCompensation;
+            if (Number(day) <= 7){
+                employer_compensation.innerHTML = (day_compensation * Number(day - 3)).toFixed(2);
+            } else {
+                employer_compensation.innerHTML = (day_compensation * 4).toFixed(2);
+            }
+            employer_daily.innerHTML = dayCompensation;
+            final_price.innerHTML = ((day_compensation * Number(day - 3)).toFixed(2));
+            if(day > 240){
                 health_daily.innerHTML = '0,00';
                 employer_daily.innerHTML = '0,00';
                 final_price.innerHTML = '0.00';
             }
-            if(num >= 4 && num <= 240){
-                let days_count = num - 3;
-                if(days_count <= 4){
-                    employer_days_compensation.innerHTML = days_count;
-                    health_days_compensation.innerHTML = '0';
-                    health_compensation.innerHTML = '0,00';
-                    health_daily.innerHTML = '0,00';
-    
-                } else if (days_count >= 5) {
-                    employer_days_compensation.innerHTML = '4';
-                    health_days_compensation.innerHTML = days_count - 4;
-                        day_compensation = health_day_compensation;
-                        if (Number(day) >= 8){
-                            health_compensation.innerHTML = (day_compensation * Number(day - 7)).toFixed(2);
-                        } 
-                    
-                    health_daily.innerHTML = health_day_compensation;
-                }
-            } else {
-                employer_days_compensation.innerHTML = '0';
-                health_days_compensation.innerHTML = '0';
-                employer_compensation.innerHTML = '0,00';
-                health_compensation.innerHTML = '0,00';
-            }
+            daysCalc(day);
         } else {
-            if(num <= 3){
+            let no_tubercolosis = Number(income * 6) / 182;
+            let deduction30 =  no_tubercolosis - (no_tubercolosis * 0.3);
+            let dayCompensation =  (deduction30 - (deduction30 * 0.2)).toFixed(2);
+            health_day_compensation = dayCompensation;
+            day_compensation = dayCompensation;
+            if (Number(day) <= 7){
+                employer_compensation.innerHTML = (day_compensation * Number(day - 3)).toFixed(2);
+            } else {
+                employer_compensation.innerHTML = (day_compensation * 4).toFixed(2);
+            }
+            employer_daily.innerHTML = dayCompensation;
+            final_price.innerHTML = ((day_compensation * Number(day - 3)).toFixed(2));
+            if(day > 182){
                 health_daily.innerHTML = '0,00';
                 employer_daily.innerHTML = '0,00';
                 final_price.innerHTML = '0.00';
             }
-            if(num >= 4 && num <= 182){
-                let days_count = num - 3;
-                if(days_count <= 4){
-                    employer_days_compensation.innerHTML = days_count;
-                    health_days_compensation.innerHTML = '0';
-                    health_compensation.innerHTML = '0,00';
-                    health_daily.innerHTML = '0,00';
+            daysCalc(day);
+        }
     
-                } else if (days_count >= 5) {
-                    employer_days_compensation.innerHTML = '4';
-                    health_days_compensation.innerHTML = days_count - 4;
-                        day_compensation = health_day_compensation;
-                        if (Number(day) >= 8){
-                            health_compensation.innerHTML = (day_compensation * Number(day - 7)).toFixed(2);
-                        } 
-                    
-                    health_daily.innerHTML = health_day_compensation;
+        function daysCalc(num){
+            if(checkbox.checked){
+                if(num <= 3){
+                    health_daily.innerHTML = '0,00';
+                    employer_daily.innerHTML = '0,00';
+                    final_price.innerHTML = '0.00';
+                }
+                if(num >= 4 && num <= 240){
+                    let days_count = num - 3;
+                    if(days_count <= 4){
+                        employer_days_compensation.innerHTML = days_count;
+                        health_days_compensation.innerHTML = '0';
+                        health_compensation.innerHTML = '0,00';
+                        health_daily.innerHTML = '0,00';
+        
+                    } else if (days_count >= 5) {
+                        employer_days_compensation.innerHTML = '4';
+                        health_days_compensation.innerHTML = days_count - 4;
+                            day_compensation = health_day_compensation;
+                            if (Number(day) >= 8){
+                                health_compensation.innerHTML = (day_compensation * Number(day - 7)).toFixed(2);
+                            } 
+                        
+                        health_daily.innerHTML = health_day_compensation;
+                    }
+                } else {
+                    employer_days_compensation.innerHTML = '0';
+                    health_days_compensation.innerHTML = '0';
+                    employer_compensation.innerHTML = '0,00';
+                    health_compensation.innerHTML = '0,00';
                 }
             } else {
-                employer_days_compensation.innerHTML = '0';
-                health_days_compensation.innerHTML = '0';
-                employer_compensation.innerHTML = '0,00';
-                health_compensation.innerHTML = '0,00';
-            }
-        }
+                if(num <= 3){
+                    health_daily.innerHTML = '0,00';
+                    employer_daily.innerHTML = '0,00';
+                    final_price.innerHTML = '0.00';
+                }
+                if(num >= 4 && num <= 182){
+                    let days_count = num - 3;
+                    if(days_count <= 4){
+                        employer_days_compensation.innerHTML = days_count;
+                        health_days_compensation.innerHTML = '0';
+                        health_compensation.innerHTML = '0,00';
+                        health_daily.innerHTML = '0,00';
         
+                    } else if (days_count >= 5) {
+                        employer_days_compensation.innerHTML = '4';
+                        health_days_compensation.innerHTML = days_count - 4;
+                            day_compensation = health_day_compensation;
+                            if (Number(day) >= 8){
+                                health_compensation.innerHTML = (day_compensation * Number(day - 7)).toFixed(2);
+                            } 
+                        
+                        health_daily.innerHTML = health_day_compensation;
+                    }
+                } else {
+                    employer_days_compensation.innerHTML = '0';
+                    health_days_compensation.innerHTML = '0';
+                    employer_compensation.innerHTML = '0,00';
+                    health_compensation.innerHTML = '0,00';
+                }
+            }
+            
+        }
+    
     }
 
 
